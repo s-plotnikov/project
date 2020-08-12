365465493522d064a6da4090a87f1fdd3d94a516
s-plotnikov <seryoga.22@mail.ru>
2020-08-12 20:32:11 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+console.log('arr' + " - object");
+console.log(4 + " - object");
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(2+ 2 *2 ==8);
+ 
+&&
+|| 
+
+const isChecked = false,
+      isClose = false;
+
+console.log(isChecked || !isClose);
